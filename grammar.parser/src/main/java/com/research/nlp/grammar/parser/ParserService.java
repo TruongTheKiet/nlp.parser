0267cd011a67fea9d981b54fa0c15f92ef8a556b
The Kiet Truong <kiet.truongthe1@nashtechglobal.com>
2021-07-05 23:47:58 +0700

    Init project

---
+++ grammar.parser/src/main/java/com/research/nlp/grammar/parser/ParserService.java
@@ -0,0 +1,10 @@
+package com.research.nlp.grammar.parser;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParserService {
+    public String process(){
+        return "{\"example\":\"value\"}";
+    }
+}
