e3c3885c51cdfc955bd6fa5e757470ebcc65d8e4
The Kiet Truong <kiet.truongthe1@nashtechglobal.com>
2021-07-06 00:46:41 +0700

    Edit rest API

--- grammar.parser/src/main/java/com/research/nlp/grammar/parser/RESTParser.java
+++ grammar.parser/src/main/java/com/research/nlp/grammar/parser/RESTParser.java
@@ -2,9 +2,11 @@ package com.research.nlp.grammar.parser;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("rest")
+@RestController()
+@RequestMapping (value = "rest")
 public class RESTParser {
     @Autowired
     private ParserService parserService;
